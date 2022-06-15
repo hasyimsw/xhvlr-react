f35bf4c87ea11b3f4abff41046b8e17fd11ae25d
Hasyim Sri Wahyudi <hasyimsriewahyudi@gmail.com>
2022-06-15 11:14:07 +0700

    update file

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,14 +1,30 @@
 import React from 'react'
-import { CloudUploadIcon, DatabaseIcon, ServerIcon, PaperAirplaneIcon } from '@heroicons/react/solid'
 import DataServices from './DataServices'
 
 const About = () => {
   return (
     <div className='w-full h-full'>
-        <div className='max-w-[1240px] pb-24 mx-auto px-4'>
+        <div className='max-w-[1240px] pb-32 mx-auto px-4'>
           {/* data services */}
           <DataServices />
-
+          <div className='pt-20 text-center'>
+            <h2 className='text-5xl font-bold'>Trusted by developers across the world</h2>
+            <p className='text-2xl py-8 text-slate-500 md:w-8/12 mx-auto'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti praesentium molestias cupiditate magni nisi provident?</p>
+          </div>
+          <div className='grid md:grid-cols-3 gap-4 mx-auto text-center'>
+            <div className='shadow-xl w-8/12 md:w-full mx-auto rounded-xl bg-slate-100 border p-4'>
+              <p className='text-6xl font-bold textPrimary'>100%</p>
+              <p className='text-slate-500 mt-2'>Completion</p>
+            </div>
+            <div className='shadow-xl w-8/12 md:w-full mx-auto rounded-xl bg-slate-300 border p-4'>
+              <p className='text-6xl font-bold textSecondary'>24/7</p>
+              <p className='text-[#082032] mt-2'>Delivery</p>
+            </div>
+            <div className='shadow-xl w-8/12 md:w-full mx-auto rounded-xl bg-slate-100 border p-4'>
+              <p className='text-6xl font-bold textPrimary'>100K</p>
+              <p className='text-slate-500 mt-2'>Transactions</p>
+            </div>
+          </div>
         </div>
     </div>
   )
--- src/components/DataServices.jsx
+++ src/components/DataServices.jsx
@@ -6,7 +6,7 @@ const DataServices = () => {
     <>
         <div className='px-4 bgPrimary text-white p-2 mt-[-70px] md:mt-[-60px] text-center lg:w-8/12 mx-auto rounded-lg border-4 border-[#ff9201] shadow-xl'>
             <h2 className='pb-4 text-lg font-bold'>Data Services</h2>
-            <div className='grid grid-cols-2 md:grid-cols-4 justify-between gap-4'>
+            <div className='grid grid-cols-2 md:grid-cols-4 justify-between md:gap-4'>
                 <p className='py-2 mx-auto flex items-center'><CloudUploadIcon className='h-6 textSecondary' /> App Security</p>
                 <p className='py-2 mx-auto flex items-center'><DatabaseIcon className='h-6 textSecondary' /> Dashboard</p>
                 <p className='py-2 mx-auto flex items-center'><ServerIcon className='h-6 textSecondary' /> Cloud Data</p>

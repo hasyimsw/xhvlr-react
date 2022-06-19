cc50a2b501f915602c6818bff74caa4935633b36
Hasyim Sri Wahyudi <hasyimsriewahyudi@gmail.com>
2022-06-19 17:00:42 +0700

    update file

---
+++ src/components/Pricing.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import {CheckIcon} from '@heroicons/react/solid'
+
+const Pricing = () => {
+  return (
+    <div className='w-full bgPrimary text-white py-20'>
+        <div>
+
+        </div>
+
+        <div className='max-w-[1240px] mx-auto px-4'>
+            <div>
+                <h2 className='text-3xl pt-8 text-slate-200 text-center'>Pricing</h2>
+                <h3 className='text-5xl font-bold py-6 text-center'>The right price for your research.</h3>
+                <p className='py-4 md:text-center text-3xl text-slate-300'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam cupiditate voluptates neque doloremque minima sit sed nam iste quos enim.</p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Pricing

228c5f59c1226c09e792e5bbaf00e92e676e575a
Hasyim Sri Wahyudi <hasyimsriewahyudi@gmail.com>
2022-06-21 19:19:45 +0700

    update file

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,20 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div></div>
+    <div className='w-full pt-20 text-slate-200 bg-slate-900 px-4'>
+      <div>
+        <div>
+          <h5>Solutions</h5>
+          <ul>
+            <li>Marketing</li>
+            <li>Analytics</li>
+            <li>Commerce</li>
+            <li>Data</li>
+            <li>Cloud</li>
+          </ul>
+        </div>
+      </div>
+    </div>
   )
 }
 
